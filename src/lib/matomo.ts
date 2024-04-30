d30d28fb05fc4558ca57b80f8d602c4b370f96bb
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2024-04-30 14:08:15 +0200

    fix: remove unneeded anon function

--- src/lib/matomo.ts
+++ src/lib/matomo.ts
@@ -9,20 +9,18 @@ function initMatomo() {
   if (!import.meta.env.PROD) return;
 
   const _paq = (window._paq = window._paq || []);
+  const u = import.meta.env.VITE_MATOMO_URL;
   _paq.push(["trackPageView"]);
   _paq.push(["enableLinkTracking"]);
-  (function () {
-    const u = import.meta.env.VITE_MATOMO_URL;
-    _paq.push(["setTrackerUrl", u + "matomo.php"]);
-    _paq.push(["setSiteId", import.meta.env.VITE_MATOMO_SITE_ID]);
-    const d = document,
-      g = d.createElement("script"),
-      s = d.getElementsByTagName("script")[0];
-    g.type = "text/javascript";
-    g.async = true;
-    g.src = u + "matomo.js";
-    s.parentNode.insertBefore(g, s);
-  })();
+  _paq.push(["setTrackerUrl", u + "matomo.php"]);
+  _paq.push(["setSiteId", import.meta.env.VITE_MATOMO_SITE_ID]);
+  const d = document,
+    g = d.createElement("script"),
+    s = d.getElementsByTagName("script")[0];
+  g.type = "text/javascript";
+  g.async = true;
+  g.src = u + "matomo.js";
+  s.parentNode.insertBefore(g, s);
 }
 
 export default initMatomo;
