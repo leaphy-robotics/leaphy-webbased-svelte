7759b213ac578fdeeba78fbdf14138f89b7ed70e
pajotg <pajotg@pajotg.pajotg>
2024-05-04 22:05:35 +0200

    fix: actually fix test formatting

--- tests/undo_redo.spec.ts
+++ tests/undo_redo.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { goToHomePage, openExample as loadExample } from "./utils";
 
 test.beforeEach(goToHomePage);
