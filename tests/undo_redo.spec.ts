21be623d60f854e2d7193f88f15e431b8e79b7b3
pajotg <pajotg@pajotg.pajotg>
2024-05-04 22:01:21 +0200

    fix: test formatting

--- tests/undo_redo.spec.ts
+++ tests/undo_redo.spec.ts
@@ -31,29 +31,29 @@ test("Undo redo - Variable change", async ({ page }) => {
 	await loadExample(page, "blink");
 
 	// Change a boolean
-	await page.getByText('true').click();
-	await page.locator('#blockly-1').getByText('false').click();
-	await expect(page.getByText('false')).toHaveCount(2);
+	await page.getByText("true").click();
+	await page.locator("#blockly-1").getByText("false").click();
+	await expect(page.getByText("false")).toHaveCount(2);
 	await page.keyboard.press("Control+z");
-	await expect(page.getByText('false')).toHaveCount(1);
+	await expect(page.getByText("false")).toHaveCount(1);
 	await page.keyboard.press("Control+y");
-	await expect(page.getByText('false')).toHaveCount(2);
+	await expect(page.getByText("false")).toHaveCount(2);
 
 	// Change one of the delays
-	await page.getByText('1000').first().click();
-	await page.getByRole('textbox').fill('123');
-	await expect(page.getByText('123', { exact: true })).toBeVisible();
+	await page.getByText("1000").first().click();
+	await page.getByRole("textbox").fill("123");
+	await expect(page.getByText("123", { exact: true })).toBeVisible();
 	await page.keyboard.press("Control+z");
-	await expect(page.getByText('123', { exact: true })).toBeHidden();
+	await expect(page.getByText("123", { exact: true })).toBeHidden();
 	await page.keyboard.press("Control+y");
-	await expect(page.getByText('123', { exact: true })).toBeVisible();
-	
-	await page.getByRole('textbox').press('Enter');
-	await expect(page.getByText('123', { exact: true })).toBeVisible();
+	await expect(page.getByText("123", { exact: true })).toBeVisible();
+
+	await page.getByRole("textbox").press("Enter");
+	await expect(page.getByText("123", { exact: true })).toBeVisible();
 	await page.keyboard.press("Control+z");
-	await expect(page.getByText('123', { exact: true })).toBeHidden();
+	await expect(page.getByText("123", { exact: true })).toBeHidden();
 	await page.keyboard.press("Control+y");
-	await expect(page.getByText('123', { exact: true })).toBeVisible();
+	await expect(page.getByText("123", { exact: true })).toBeVisible();
 });
 
 test("Undo redo - Dragging", async ({ page }) => {
@@ -65,13 +65,15 @@ test("Undo redo - Dragging", async ({ page }) => {
 	await page.locator(".side").first().click(); // Open code
 
 	// Drag the repeat block somewhere to the left, disconnecting it
-	await page.getByText('repeat forever').dragTo(page.getByText('repeat forever'), {
-		force: true,
-		targetPosition: {
-			x: -250,
-			y: 0,
-		}
-	});
+	await page
+		.getByText("repeat forever")
+		.dragTo(page.getByText("repeat forever"), {
+			force: true,
+			targetPosition: {
+				x: -250,
+				y: 0,
+			},
+		});
 
 	await expect(page.locator(".view-lines")).not.toContainText("delay");
 
@@ -81,13 +83,15 @@ test("Undo redo - Dragging", async ({ page }) => {
 	await expect(page.locator(".view-lines")).not.toContainText("delay");
 
 	// Move it back to its original location, connecting it again
-	await page.getByText('repeat forever').dragTo(page.getByText('Leaphy', { exact: true }), {
-		force: true,
-		targetPosition: {
-			x: 0,
-			y: 50,
-		}
-	});
+	await page
+		.getByText("repeat forever")
+		.dragTo(page.getByText("Leaphy", { exact: true }), {
+			force: true,
+			targetPosition: {
+				x: 0,
+				y: 50,
+			},
+		});
 
 	await expect(page.locator(".view-lines")).toContainText("delay");
 	await page.keyboard.press("Control+z");
