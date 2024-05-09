3befc0ae43c378dc0aaff39950ec2de35435e170
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2024-05-09 16:14:21 +0200

    feat: add Sentry-based user feedback form

---
+++ src/lib/components/core/popups/popups/Feedback.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+import Button from "$components/ui/Button.svelte";
+import TextArea from "$components/ui/TextArea.svelte";
+import TextInput from "$components/ui/TextInput.svelte";
+import { type PopupState, popups } from "$state/popup.svelte";
+import * as Sentry from "@sentry/browser";
+import { getContext } from "svelte";
+import { _ } from "svelte-i18n";
+import type { Writable } from "svelte/store";
+
+interface Props {
+	comments?: string;
+	senderName: string;
+	senderEmail: string;
+}
+let {
+	comments = $bindable(""),
+	senderName = $bindable(""),
+	senderEmail = $bindable(""),
+}: Props = $props();
+const popupState = getContext<Writable<PopupState>>("state");
+
+function cancel() {
+	popups.close($popupState.id, false);
+}
+
+function save() {
+	const eventId = Sentry.captureMessage("User Feedback");
+	const userFeedback = {
+		event_id: eventId,
+		name: senderName,
+		email: senderEmail,
+		comments: comments,
+	};
+	Sentry.captureUserFeedback(userFeedback);
+
+	popups.close($popupState.id, comments);
+}
+
+function onsubmit(event: SubmitEvent) {
+	event.preventDefault();
+	save();
+}
+</script>
+
+<form class="content" {onsubmit}>
+    <h2>{$_("FEEDBACK")}</h2>
+	<div class="input">
+		<TextInput
+			placeholder={$_("NAME")}
+			bind:value={senderEmail}
+			mode="secondary"
+			rounded={false}>
+		</TextInput>
+	</div>
+
+	<div class="input">
+		<TextInput
+			placeholder={$_("EMAIL")}
+			bind:value={senderName}
+			mode="secondary"
+			rounded={false}>
+		</TextInput>
+	</div>
+
+    <TextArea
+        bind:value={comments}
+        mode={"secondary"}
+        rounded={false}
+        focus={true}
+		rows={10}
+    />
+    <div class="actions">
+        <Button onclick={cancel} mode={"secondary"} name={$_("CANCEL")} />
+        <Button onclick={save} mode={"primary"} name={$_("SEND")} />
+    </div>
+</form>
+
+<style>
+	.input {
+		padding-bottom: 10px;
+	}
+
+    .content {
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        min-width: 400px;
+        text-align: center;
+    }
+
+    .actions {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 20px;
+    }
+</style>
---
+++ src/lib/components/ui/TextArea.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+import { onMount } from "svelte";
+
+interface Props {
+	value: string;
+	mode: "primary" | "secondary";
+	rounded: boolean;
+	focus?: boolean;
+	rows: number;
+}
+let { value = $bindable(""), mode, rounded, focus, rows }: Props = $props();
+
+let input: HTMLTextAreaElement;
+onMount(() => {
+	if (focus) input.focus();
+});
+</script>
+
+<textarea
+    bind:this={input}
+    class="textarea"
+    bind:value
+	rows={rows}
+    class:primary={mode === "primary"}
+    class:secondary={mode === "secondary"}
+    class:rounded
+></textarea>
+
+<style>
+    .textarea {
+        border: none;
+        padding: 5px 10px;
+        margin: 0;
+        width: 100%;
+        outline: 0;
+        font-size: 1em;
+    }
+
+    .primary {
+        background: var(--primary);
+        color: var(--on-primary);
+    }
+    .secondary {
+        background: var(--secondary);
+        color: var(--on-secondary);
+    }
+    .rounded {
+        border-radius: 20px;
+    }
+
+    .primary::placeholder {
+        color: var(--text-muted);
+    }
+    .secondary::placeholder {
+        color: var(--on-secondary-muted);
+    }
+</style>
