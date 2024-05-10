b4c5abb147d08dafb74e7e14c1c643deec6823ff
pajotg <33227855+pajotg@users.noreply.github.com>
2024-05-10 14:45:06 +0200

    test: loading of old XML and new JSON files and saving to current file

---
+++ tests/load_workspace.spec.ts
@@ -0,0 +1,48 @@
+import { FileChooser, expect, test } from "@playwright/test";
+import { goToHomePage, setNextOpenedFile } from "./utils";
+
+test.beforeEach(goToHomePage);
+
+let test_files = [
+	["Old Xml format", "hello_world_xml.l_original_uno"],
+	["New Json format", "hello_world_json.l_original_uno"],
+];
+
+for (let [testName, file] of test_files) {
+	test(`LoadAndSave - ${testName}`, async ({ page }) => {
+		await page.getByText("Leaphy Original").click();
+		await page.getByText("Original Uno").click();
+
+		await page.getByRole("button", { name: "My projects" }).click();
+
+		// Playwright doesn't seem to support `showOpenFilePicker()` so mock it
+		let createOnWritePromise = await setNextOpenedFile(
+			page,
+			`./tests/saved_workspaces/${file}`,
+		);
+
+		await page.getByRole("cell", { name: "Open" }).click();
+
+		// All of these should be loaded in
+		await expect(page.getByText("repeat")).toBeVisible();
+		await expect(page.getByText("during")).toBeVisible();
+		await expect(page.getByText("hello world!")).toBeVisible();
+		await expect(page.getByText("10", { exact: true })).toBeVisible();
+
+		// The code should have also been updated
+		await page.locator(".side").first().click();
+
+		await expect(page.locator(".view-lines")).toContainText("test = 10");
+		await expect(page.locator(".view-lines")).toContainText("delay(1000)");
+		await expect(page.locator(".view-lines")).toContainText('"hello world!"');
+
+		// Save the project, it should write to the previously opened file!
+		await page.getByRole("button", { name: "My projects" }).click();
+
+		let onWritePromise = createOnWritePromise();
+		await page.getByRole("cell", { name: "Save", exact: true }).click();
+		let written = await onWritePromise;
+
+		expect(written).toContain("leaphy_serial_print_line"); // check that it contains the print
+	});
+}
--- tests/utils.ts
+++ tests/utils.ts
@@ -1,3 +1,4 @@
+import { promises as fs } from "node:fs";
 import type { Page, PlaywrightTestArgs } from "@playwright/test";
 
 export async function goToHomePage({ page }: PlaywrightTestArgs) {
@@ -10,4 +11,69 @@ export async function openExample(page: Page, example: string | RegExp) {
 	await page.getByRole("button", { name: "My projects" }).click();
 	await page.getByRole("cell", { name: "Examples" }).click();
 	await page.getByRole("button", { name: example }).click();
+}
+
+// Playwright doesn't seem to support `showOpenFilePicker()` so this functions mocks it
+export async function setNextOpenedFile(
+	page: Page,
+	path: string,
+): Promise<(timeout?: number) => Promise<string>> {
+	let content = await fs.readFile(path, "utf-8");
+
+	let onWriteResolve: ((data: string) => void) | undefined = undefined;
+
+	await page.exposeFunction("onWriteOpenedFile", (data: string) => {
+		if (onWriteResolve !== undefined) {
+			onWriteResolve(data);
+		}
+	});
+
+	await page.evaluate(
+		([content, file_name]) => {
+			window.showOpenFilePicker = async (): Promise<[FileSystemFileHandle]> => {
+				// return something close enough to what a [FileSystemFileHandle] looks like, as you can't actually create it, atleast i could not find it
+				return [
+					{
+						getFile: async () => {
+							return {
+								text: async () => content,
+								name: file_name,
+							};
+						},
+						createWritable: async () => {
+							let written = "";
+
+							return {
+								write: async (data: FileSystemWriteChunkType) => {
+									// There has to be a better way right??
+									if (typeof data === "string") {
+										throw "todo";
+									}
+									if ("data" in data && typeof data.data === "string") {
+										written += data.data;
+									} else {
+										throw "todo";
+									}
+								},
+								close: async () => {
+									await (window as any).onWriteOpenedFile(written);
+								},
+							};
+						},
+						name: file_name,
+					},
+				] as unknown as [FileSystemFileHandle];
+			};
+		},
+		[content, path],
+	);
+
+	return (timeout = 1000) => {
+		return new Promise<string>((resolve, reject) => {
+			onWriteResolve = resolve;
+			setTimeout(() => {
+				reject("File not written within time!");
+			}, timeout);
+		});
+	};
 }
