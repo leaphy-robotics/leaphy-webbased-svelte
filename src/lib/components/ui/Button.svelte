172801870e74ee351f3bdc506cd7458fe243ffd4
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2024-05-11 15:50:18 +0200

    chore: reformat button

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -15,6 +15,7 @@ interface Props {
 	bold?: boolean;
 	type?: "button" | "submit";
 }
+
 const {
 	name,
 	mode,
@@ -29,6 +30,7 @@ const {
 let btn: HTMLButtonElement = $state();
 
 const open = writable(false);
+
 function onContext() {
 	if (!context) return;
 
@@ -37,70 +39,74 @@ function onContext() {
 </script>
 
 {#if $open}
-    <ContextMenu {open} source={btn} content={context} />
+	<ContextMenu {open} source={btn} content={context}/>
 {/if}
 
 <button
-    bind:this={btn}
-    {onclick}
-    {disabled}
+	bind:this={btn}
+	{onclick}
+	{disabled}
 	{type}
-    class="btn"
-    class:primary={mode === "primary"}
-    class:secondary={mode === "secondary"}
-    class:outlined={mode === "outlined"}
-    class:accent={mode === "accent"}
-    class:bold
+	class="btn"
+	class:primary={mode === "primary"}
+	class:secondary={mode === "secondary"}
+	class:outlined={mode === "outlined"}
+	class:accent={mode === "accent"}
+	class:bold
 >
-    {#if icon}
-        {#if typeof icon === "string"}
-            <img class="icon" src={icon} alt="Icon" />
-        {:else}
-            <Fa {icon} />
-        {/if}
-    {/if}
-    {#if name}{name}{/if}
+	{#if icon}
+		{#if typeof icon === "string"}
+			<img class="icon" src={icon} alt="Icon"/>
+		{:else}
+			<Fa {icon}/>
+		{/if}
+	{/if}
+	{#if name}{name}{/if}
 </button>
 
 <style>
-    .btn {
-        display: flex;
-        gap: 5px;
-        align-items: center;
-        cursor: pointer;
-        border-radius: 20px;
-        background: none;
-        border: none;
-        padding: 10px 15px;
-    }
+	.btn {
+		display: flex;
+		gap: 5px;
+		align-items: center;
+		cursor: pointer;
+		border-radius: 20px;
+		background: none;
+		border: none;
+		padding: 10px 15px;
+	}
 
-    .icon {
-        height: 1em;
-    }
+	.icon {
+		height: 1em;
+	}
 
-    .primary {
-        background: var(--primary);
-        color: var(--on-primary);
-    }
-    .secondary {
-        background: var(--secondary);
-        color: var(--on-secondary);
-    }
-    .outlined {
-        border: 1px solid var(--accent);
-        color: var(--on-primary);
-    }
-    .accent {
-        background: var(--accent);
-        color: var(--on-accent);
-    }
-    .bold {
-        font-weight: bolder;
-        font-size: 1.1em;
-    }
+	.primary {
+		background: var(--primary);
+		color: var(--on-primary);
+	}
 
-    .btn[disabled] {
-        filter: opacity(.5);
-        cursor: unset;
-    }
+	.secondary {
+		background: var(--secondary);
+		color: var(--on-secondary);
+	}
+
+	.outlined {
+		border: 1px solid var(--accent);
+		color: var(--on-primary);
+	}
+
+	.accent {
+		background: var(--accent);
+		color: var(--on-accent);
+	}
+
+	.bold {
+		font-weight: bolder;
+		font-size: 1.1em;
+	}
+
+	.btn[disabled] {
+		filter: opacity(.5);
+		cursor: unset;
+	}
 </style>
