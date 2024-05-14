7b8a879680c2058c1ee7ed3d4014e83676bf5b32
pajotg <33227855+pajotg@users.noreply.github.com>
2024-05-14 09:26:33 +0200

    Added tests to load the old XML and new JSON format in blockly workspaces (#59)
    * test: loading of old XML and new JSON files and saving to current file

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,3 +1,4 @@
+import { promises as fs } from "node:fs";
 import type { Page, PlaywrightTestArgs } from "@playwright/test";
 
 export async function goToHomePage({ page }: PlaywrightTestArgs) {
@@ -10,4 +11,64 @@ export async function openExample(page: Page, example: string | RegExp) {
 	await page.getByRole("button", { name: "My projects" }).click();
 	await page.getByRole("cell", { name: "Examples" }).click();
 	await page.getByRole("button", { name: example }).click();
+}
+
+// Playwright doesn't seem to support `showOpenFilePicker()` so this functions mocks it
+export async function mockShowOpenFilePicker(
+	page: Page,
+	path: string,
+): Promise<Promise<(timeout?: number) => Promise<FileSystemWriteChunkType[]>>> {
+	let content = await fs.readFile(path, "utf-8");
+
+	await page.evaluate(
+		([content, path]) => {
+			const blob = new Blob([content], { type: "application/json" });
+			const file = new File([blob], path, { type: "application/json" });
+
+			const createWritable = async () => {
+				let writtenChunks: FileSystemWriteChunkType[] = [];
+				return {
+					write: async (data: FileSystemWriteChunkType) => {
+						writtenChunks.push(data);
+					},
+					close: async () => {
+						await (window as any).onWriteOpenedFile(writtenChunks);
+					},
+				};
+			};
+
+			const fileHandle = {
+				getFile: async () => file,
+				name: path,
+				createWritable,
+			};
+
+			(window as any).showOpenFilePicker = async () => {
+				return [fileHandle];
+			};
+		},
+		[content, path],
+	);
+
+	let createOnWritePromise = async (timeout = 1000) => {
+		let onWriteResolve:
+			| ((writtenChunks: FileSystemWriteChunkType[]) => void)
+			| undefined = undefined;
+
+		await page.exposeFunction(
+			"onWriteOpenedFile",
+			(writtenChunks: FileSystemWriteChunkType[]) => {
+				if (onWriteResolve) onWriteResolve(writtenChunks);
+			},
+		);
+
+		return new Promise<FileSystemWriteChunkType[]>((resolve, reject) => {
+			onWriteResolve = resolve;
+			setTimeout(() => {
+				reject("File not written within time!");
+			}, timeout);
+		});
+	};
+
+	return createOnWritePromise;
 }
