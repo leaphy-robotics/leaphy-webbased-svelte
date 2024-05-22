aec23a5c3876618ffd981b0510c92c0183bbe871
sverben <59171289+sverben@users.noreply.github.com>
2024-05-22 17:05:20 +0200

    feat: add tooltips

--- src/lib/components/ui/SideButton.svelte
+++ src/lib/components/ui/SideButton.svelte
@@ -1,20 +1,43 @@
 <script lang="ts">
 import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import Fa from "svelte-fa";
+import { _ } from "svelte-i18n";
 
 interface Props {
 	icon: IconDefinition;
+	action: string;
 	onclick: () => void;
 }
-const { icon, onclick }: Props = $props();
+const { icon, action, onclick }: Props = $props();
 </script>
 
 <button class="side" {onclick}>
+    <div class="tooltip">{$_(action)}</div>
     <Fa {icon} />
 </button>
 
 <style>
+    .tooltip {
+        position: absolute;
+        right: 100%;
+        margin-right: 15px;
+        background: var(--secondary);
+        color: var(--on-secondary);
+        padding: 10px;
+        border-radius: 5px;
+        font-size: 14px;
+        pointer-events: none;
+        filter: opacity(0);
+        transition: .3s ease;
+        width: 100px;
+    }
+
+    .side:hover .tooltip {
+        filter: opacity(1);
+    }
+
     .side {
+        position: relative;
         cursor: pointer;
         font-size: 20px;
         height: 66px;
