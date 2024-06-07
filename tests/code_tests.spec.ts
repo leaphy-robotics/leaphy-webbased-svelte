0f247407deb0d7aa9d2736bc30bd22f384fffcf8
pajotg <33227855+pajotg@users.noreply.github.com>
2024-06-07 14:51:58 +0200

    test: added tests to ensure that various blocks are producing the correct code

---
+++ tests/code_tests.spec.ts
@@ -0,0 +1,42 @@
+import { promises as fs } from "node:fs";
+import { type Page, expect, test } from "@playwright/test";
+import { goToHomePage, mockShowOpenFilePicker, openExample } from "./utils";
+
+test.beforeEach(goToHomePage);
+
+async function testExtension(page: Page, extension: string) {
+	await page.getByRole("button", { name: "Code" }).nth(1).click();
+
+	const files = await fs.readdir("./tests/code_tests");
+	for (const workspace_file of files) {
+		if (!workspace_file.endsWith(extension)) {
+			continue;
+		}
+
+		await page.getByRole("button", { name: "My projects" }).click();
+
+		await mockShowOpenFilePicker(page, `./tests/code_tests/${workspace_file}`);
+
+		await page.getByRole("cell", { name: "Open" }).click();
+
+		let code = (
+			await fs.readFile(`./tests/code_tests/${workspace_file}_code`)
+		).toString();
+
+		for (const line of code.split("\n")) {
+			await expect(page.locator(".view-lines")).toContainText(line);
+		}
+	}
+}
+
+test("Code blocks - Leaphy Click", async ({ page }) => {
+	await page.getByText("Leaphy Click").click();
+	await testExtension(page, ".l_click");
+});
+
+test("Code blocks - Original Uno", async ({ page }) => {
+	await page.getByText("Leaphy Original").click();
+	await page.getByText("Original Uno").click();
+
+	await testExtension(page, ".l_original_uno");
+});
