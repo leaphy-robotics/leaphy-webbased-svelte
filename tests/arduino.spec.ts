c985afcb5ea886f2982c18182647508a3575832d
pajotg <33227855+pajotg@users.noreply.github.com>
2024-06-14 13:27:26 +0200

    test: Made Arduino tests faster & included more logging (#82)
    test: made Arduino tests faster when they fail and added logging of window.avrdudeLog to help debugging

--- tests/arduino.spec.ts
+++ tests/arduino.spec.ts
@@ -1,6 +1,8 @@
 import { expect, test } from "@playwright/test";
 import { goToHomePage, openExample, setupArduino } from "./utils";
 
+test.describe.configure({ mode: "serial" });
+
 test.beforeEach(setupArduino);
 test.beforeEach(goToHomePage);
 
@@ -12,12 +14,15 @@ test("Arduino - Blockly upload", async ({ page }) => {
 
 	await page.getByRole("button", { name: "Upload to robot" }).click();
 	await expect(
-		page.getByRole("heading", { name: "Robot update complete" }),
+		page.getByRole("button", { name: "Go back to editor" }),
 	).toBeVisible({
 		timeout: 15000,
 	});
-
-	await page.getByRole("button", { name: "Go back to editor" }).click();
+	await expect(
+		page.getByRole("heading", { name: "Robot update complete" }),
+	).toBeVisible({
+		timeout: 50, // It should already be visible
+	});
 });
 
 test("Arduino - C++ upload", async ({ page }) => {
@@ -25,10 +30,20 @@ test("Arduino - C++ upload", async ({ page }) => {
 
 	await page.getByRole("button", { name: "Upload to robot" }).click();
 	await expect(
-		page.getByRole("heading", { name: "Robot update complete" }),
+		page.getByRole("button", { name: "Go back to editor" }),
 	).toBeVisible({
 		timeout: 15000,
 	});
+	await expect(
+		page.getByRole("heading", { name: "Robot update complete" }),
+	).toBeVisible({
+		timeout: 50, // It should already be visible
+	});
+});
 
-	await page.getByRole("button", { name: "Go back to editor" }).click();
+// Help debug errors in the above tests
+test.afterEach(async ({ page }) => {
+	await page.evaluate(
+		"console.log(JSON.stringify(window.avrdudeLog, null, 4))",
+	);
 });
