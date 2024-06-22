5365754c2dea436d3b45e5439dc413117ca4e1d2
sverben <59171289+sverben@users.noreply.github.com>
2024-06-22 10:32:41 +0200

    fix: nog een keer

--- tests/blink_example.spec.ts
+++ tests/blink_example.spec.ts
@@ -25,7 +25,7 @@ test("Load Blink example and check code", async ({ page }) => {
 
 	// Check that the delays have been updated accordingly
 	await expect(page.locator(".view-lines")).toContainText(
-		"delay(500); digitalWrite(13, LOW); delay(500);",
+		"delay(500); digitalWrite(13, false); delay(500);",
 	);
 
 	// Save the robot
