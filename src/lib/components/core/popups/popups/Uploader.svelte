61254988e67e5fac0979c81f794edd2c1ab3a022
koen1711 <koenvurk1711@gmail.com>
2024-09-18 20:22:03 +0200

    feat: add DriverInstall.svelte help and button to Uploader.svelte

--- src/lib/components/core/popups/popups/Uploader.svelte
+++ src/lib/components/core/popups/popups/Uploader.svelte
@@ -14,6 +14,8 @@ import {
 } from "$state/workspace.svelte";
 import { getContext, onMount } from "svelte";
 import type { Writable } from "svelte/store";
+import JSZip from "jszip";
+import DriverInstall from "$components/core/popups/popups/DriverInstall.svelte";
 
 interface Props {
 	source?: string;
@@ -118,6 +120,35 @@ async function connectUSB() {
 		}),
 	);
 }
+
+async function downloadDrivers() {
+	const response = await fetch(
+		"https://api.github.com/repos/leaphy-robotics/leaphy-firmware/contents/drivers",
+	);
+	const data = await response.json();
+	const files = data.map(({ download_url }) => download_url);
+	const zip = new JSZip();
+
+	await Promise.all(
+		files.map(async (url) => {
+			const res = await fetch(url);
+			zip.file(url.split("/").pop(), await res.blob());
+		}),
+	);
+
+	const a = document.createElement("a");
+	const url = URL.createObjectURL(await zip.generateAsync({ type: "blob" }));
+	a.href = url;
+	a.download = "leaphy-drivers.zip";
+	//a.click();
+	URL.revokeObjectURL(url);
+	close();
+	await popups.open({
+		component: DriverInstall,
+		data: {},
+		allowInteraction: true,
+	});
+}
 </script>
 
 <div class="content" class:error={!!failed}>
@@ -140,6 +171,13 @@ async function connectUSB() {
         {:else}
             <ProgressBar {progress} />
         {/if}
+		{#if failed}
+			<Button
+				name={$_("DOWNLOAD_DRIVERS")}
+				mode={"accent"}
+				onclick={downloadDrivers}
+			/>
+		{/if}
     {/if}
 </div>
 
