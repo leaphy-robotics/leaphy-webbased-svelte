aefef4a69dba305780e94fb0182d0611686adc3a
Koen <98043234+koen1711@users.noreply.github.com>
2025-03-15 10:39:18 +0100

    chore: perfect starling env (#175)
    * chore: remove motor blocks from starling env, move starling to center on landing page
    
    * chore: run biome
    
    * fix: also remove servo motor from original env

--- src/lib/domain/blockly/toolbox.ts
+++ src/lib/domain/blockly/toolbox.ts
@@ -185,12 +185,20 @@ export default [
 					},
 				},
 				{
+					robots: [
+						...robotGroups.ALL,
+						...robotGroups.L_STARLING_ALL.map((e) => -e),
+					],
 					type: "leaphy_original_set_motor",
 					inputs: {
 						MOTOR_SPEED: number(100),
 					},
 				},
 				{
+					robots: [
+						...robotGroups.ALL,
+						...robotGroups.L_STARLING_ALL.map((e) => -e),
+					],
 					type: "leaphy_original_move_motors",
 					inputs: {
 						MOTOR_SPEED: number(100),
@@ -198,14 +206,20 @@ export default [
 				},
 				{
 					type: "leaphy_original_servo_set",
-					robots: robotGroups.L_NANO_ALL,
+					robots: [
+						...robotGroups.L_NANO_ALL,
+						...robotGroups.L_ORIGINAL_NANO_ALL.map((e) => -e),
+					],
 					inputs: {
 						SPEED: number(100),
 					},
 				},
 				{
 					type: "leaphy_original_servo_move",
-					robots: robotGroups.L_NANO_ALL,
+					robots: [
+						...robotGroups.L_NANO_ALL,
+						...robotGroups.L_ORIGINAL_NANO_ALL.map((e) => -e),
+					],
 					inputs: {
 						SPEED: number(100),
 					},
