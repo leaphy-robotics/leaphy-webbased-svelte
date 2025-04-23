63657d915533f2e4338bda07e8f4a1e5104d950a
pajotg <33227855+pajotg@users.noreply.github.com>
2025-04-23 23:40:27 +0200

    test: checking that default sonar pins are correct for the Uno and Nano

---
+++ packages/client/tests/sonar_pins.spec.ts
@@ -0,0 +1,34 @@
+import { type Page, expect, test } from "@playwright/test";
+import { goToHomePage, selectRobot } from "./utils";
+
+test.beforeEach(goToHomePage);
+
+async function test_sonar_pins(
+	page: Page,
+	first_pin: string,
+	second_pin: string,
+) {
+	let values = page.locator(".blocklyDraggable")
+		.filter({
+			has: page.getByText('Get distance Trig')
+		})
+		.first()
+		.locator(".blocklyEditableText");
+	
+	expect(values.nth(0)).toHaveText(first_pin);
+	expect(values.nth(1)).toHaveText(second_pin);
+}
+
+test("Pins - Uno", async ({ page }) => {
+	await selectRobot(page, "Arduino Uno");
+ 
+
+	await test_sonar_pins(page, "8", "7");
+});
+
+test("Pins - Nano", async ({ page }) => {
+	await selectRobot(page, "Arduino Nano", "Arduino Nano ESP32");
+
+	await test_sonar_pins(page, "17", "16");
+});
+
