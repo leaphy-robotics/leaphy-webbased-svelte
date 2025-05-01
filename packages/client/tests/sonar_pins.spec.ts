9c978cb4576ce4bc4abbcfdab0ed297f516aebb8
pajotg <33227855+pajotg@users.noreply.github.com>
2025-05-01 15:38:46 +0200

    test: sonar pins are tested on Original Nano and Starling and corrected pins on Arduino Nano

--- packages/client/tests/sonar_pins.spec.ts
+++ packages/client/tests/sonar_pins.spec.ts
@@ -1,5 +1,5 @@
 import { type Page, expect, test } from "@playwright/test";
-import { goToHomePage, selectRobot } from "./utils";
+import { goToHomePage, newProject, selectRobot } from "./utils";
 
 test.beforeEach(goToHomePage);
 
@@ -20,14 +20,19 @@ async function test_sonar_pins(
 	expect(values.nth(1)).toHaveText(second_pin);
 }
 
-test("Pins - Uno", async ({ page }) => {
+test("SonarPins", async ({ page }) => {
 	await selectRobot(page, "Arduino Uno");
+	await test_sonar_pins(page, "7", "8");
 
-	await test_sonar_pins(page, "8", "7");
-});
-
-test("Pins - Nano", async ({ page }) => {
+	await newProject(page);
 	await selectRobot(page, "Arduino Nano", "Arduino Nano ESP32");
+	await test_sonar_pins(page, "17", "16");
 
+	await newProject(page);
+	await selectRobot(page, "Leaphy Original", "Original Nano");
+	await test_sonar_pins(page, "17", "16");
+
+	await newProject(page);
+	await selectRobot(page, "Leaphy Starling", "Starling Nano");
 	await test_sonar_pins(page, "17", "16");
 });
