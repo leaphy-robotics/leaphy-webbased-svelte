75836df689af3d9fb46f3702cc30555c0dfdff3a
LesleyMatlala <mmadiralesley16@gmail.com>
2025-11-26 14:13:45 +0200

    Adding PWM pins 44, 45, 46 in fields.ts

--- packages/client/src/lib/domain/blockly/fields.ts
+++ packages/client/src/lib/domain/blockly/fields.ts
@@ -79,6 +79,7 @@ export default class PinSelectorField extends FieldDropdown {
 				
 
 				PinSelectorField.pwmPinOptions = [
+					
                       ...PinSelectorField.generatePinRange(2, 13),
        				  ...PinSelectorField.generatePinRange(44, 46),
 				];
