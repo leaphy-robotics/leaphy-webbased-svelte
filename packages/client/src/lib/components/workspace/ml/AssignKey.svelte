16ba9356ca9c9c15a0cb984841d9b55970562ea0
sverben <59171289+sverben@users.noreply.github.com>
2026-01-29 17:00:29 +0100

    fix: implement key translations

--- packages/client/src/lib/components/workspace/ml/AssignKey.svelte
+++ packages/client/src/lib/components/workspace/ml/AssignKey.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 import type { Class } from "@leaphy-robotics/leaphy-blocks/src/categories/ml";
+import { _ } from "svelte-i18n";
 
 interface Props {
 	classData: Class;
@@ -24,10 +25,26 @@ function abort() {
 
 	binding = false;
 }
+
+function getKeyLabel(key: string): [string, Record<string, string>] | [string] {
+	if (!key) return ['UNASSIGNED'];
+	if (key.startsWith("Key")) {
+		return ['KEY', { key: key.slice(3) }]
+	}
+	if (key.startsWith("Digit")) {
+		return ['DIGIT', { key: key.slice(5) }]
+	}
+	
+	return [key.toUpperCase()]
+}
+
+let label = $derived(getKeyLabel(classData.key));
+let translation = $derived(label[0]);
+let values = $derived(label[1]);
 </script>
 
 <svelte:document onkeyup={onKey} onmousedown={abort} />
-<button onclick={assign} class="key" class:active={binding}>{binding ? 'Press any key' : classData.key || 'Unassigned'}</button>
+<button onclick={assign} class="key" class:active={binding}>{binding ? $_("PRESS_ANY_KEY") : $_(translation, { values })}</button>
 
 <style>
 	.key {
