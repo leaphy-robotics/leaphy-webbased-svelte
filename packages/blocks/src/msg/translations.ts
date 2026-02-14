f1637316e8695a51e74a7d92be6b8838dd332b0f
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2026-02-14 13:40:08 +0100

    feat: add ukrainian translation (#389)

--- packages/blocks/src/msg/translations.ts
+++ packages/blocks/src/msg/translations.ts
@@ -1,7 +1,9 @@
 import messagesEn from "./translations/en";
 import messagesNl from "./translations/nl";
+import messagesUa from "./translations/ua";
 
 export default {
 	nl: messagesNl,
 	en: messagesEn,
+	ua: messagesUa,
 };
