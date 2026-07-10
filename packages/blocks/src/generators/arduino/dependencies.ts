21fafa16b664ab483a5a2f70815c9fcde6859217
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2026-07-10 20:22:19 +0200

    chore: update leaphy extensions

--- packages/blocks/src/generators/arduino/dependencies.ts
+++ packages/blocks/src/generators/arduino/dependencies.ts
@@ -1,5 +1,5 @@
 export enum Dependencies {
-	LEAPHY_EXTENSIONS = "Leaphy Extensions@1.2.1",
+	LEAPHY_EXTENSIONS = "Leaphy Extensions@1.2.3",
 	ARDUINO_BLE = "ArduinoBLE@1.4.0",
 	LIST = "List@3.0.1",
 
